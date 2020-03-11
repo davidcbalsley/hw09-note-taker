dbaba84d70a2deb3e4e84755ed44c18d4284843b
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-03-10 23:20:33 -0500

    POST to /api/notes

--- server.js
+++ server.js
@@ -16,24 +16,47 @@ app.use(express.static('public'));
 
 // Routes
 // =============================================================
+
+// GET /notes -- return the notes.html file
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
   });
 
-// Displays all notes
+// GET /api/notes -- return all saved notes as JSON
 app.get("/api/notes", function(req, res) {
     return res.sendFile(path.join(__dirname, "db/db.json"));
   });
 
+// GET * -- return the index.html file
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
   });
 
+// POST /api/notes -- add new note to db.json file, then return the new note to the client
 app.post("/api/notes", function(req, res) {
     // Get new note from req.body
     const newNote = req.body;
 
-    console.log(newNote);
+    // Add new note to the db.json file
+
+    // Read in the db.json file
+    fs.readFile("db/db.json", (err, data) => {
+      if (err) throw err;
+      
+      // Get an array of notes from data
+      const noteList = JSON.parse(data);
+
+      // Add the new note to the list of notes
+      noteList.push(req.body);
+      
+      // Write the updated list of notes back to db.json
+      fs.writeFile("db/db.json", JSON.stringify(noteList), (err) => {
+        if (err) throw err;
+      });
+    });
+
+    // Return the new note to the client
+    res.json(req.body);
 });
 
 // Starts the server to begin listening
